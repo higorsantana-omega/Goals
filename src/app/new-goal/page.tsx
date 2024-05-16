0590ad897b7433fede9f40a0f2dd8d7b8d6f165c
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-15 22:33:27 -0300

    feat: add redirect to page new-goal

---
+++ src/app/new-goal/page.tsx
@@ -0,0 +1,7 @@
+export default function NewGoalPage () {
+  return (
+    <div>
+      <h1>New Goal Page</h1>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/D5VQwkeGYFk
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
-import Link from "next/link"
+'use client'
+
 import { Button } from "@/components/ui/button"
-import { DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuContent, DropdownMenu } from "@/components/ui/dropdown-menu"
-import { Progress } from "@/components/ui/progress"
 import { CardContent, CardDate, CardDescription, CardFooter, CardIcon, CardRoot, CardTitle } from "@/components/Card"
-import { Header } from "@/components/Header"
 import { PlusIcon } from "@radix-ui/react-icons"
+import { useRouter } from 'next/navigation'
 
 export default function Component() {
+  const router = useRouter()
+
   return (
     <>
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Our Goals</h2>
         <div className="flex items-center gap-2">
-          <Button size="sm" variant="outline">
+          <Button size="sm" variant="outline" onClick={() => router.push('/new-goal')}>
             <PlusIcon className="mr-2 h-4 w-4" />
             Add goal
           </Button>
