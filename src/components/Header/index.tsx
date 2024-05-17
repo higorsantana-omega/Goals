f717eddc2597c8c5178668cd5c6f42549d1783e3
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-17 13:46:23 -0300

    feat: change logo and favicon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuContent, DropdownMenu } from "@/components/ui/dropdown-menu"
-import { SVGProps } from "react"
+import { Logo } from "../Logo"
 
 export function Header () {
   return (
     <header className="flex items-center justify-between h-16 px-4 md:px-6 border-b shrink-0">
       <div className="flex items-center gap-6">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-6 w-6" />
+          <Logo className="h-6 w-6" />
           <span className="sr-only">Goals</span>
         </Link>
         <nav className="hidden md:flex items-center gap-6">
@@ -39,24 +39,5 @@ export function Header () {
         </DropdownMenu>
       </div>
     </header>
-  )
-}
-
-function MountainIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   )
 }
