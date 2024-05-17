3daa957ed05556c2ed8a856e45c043dcf199a78c
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-16 23:00:15 -0300

    feat: add mount stepper in page new-goal

--- src/app/new-goal/page.tsx
+++ src/app/new-goal/page.tsx
@@ -1,7 +1,32 @@
+import { Stepper } from "@/components/Stepper";
+import { AdditionalCustomization } from "@/components/Steps/AdditionalCustomization";
+import { GoalDetails } from "@/components/Steps/GoalDetails";
+import { PlanningResources } from "@/components/Steps/PlanningResources";
+import { TrackingCommunication } from "@/components/Steps/TrackingCommunication";
+
 export default function NewGoalPage () {
   return (
-    <div>
-      <h1>New Goal Page</h1>
+    <div className="min-h-screen flex justify-center">
+      <Stepper 
+        steps={[
+          {
+            label: 'Goal Details',
+            content: <GoalDetails />
+          },
+          {
+            label: 'Planning and Resources',
+            content: <PlanningResources />
+          },
+          {
+            label: 'Tracking and Communication',
+            content: <TrackingCommunication />
+          },
+          {
+            label: 'Additional Customization',
+            content: <AdditionalCustomization />
+          }
+        ]}
+      />
     </div>
   )
 }
