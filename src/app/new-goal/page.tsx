c8c154518f79df2972ee5c6d76e1459658a757b8
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-18 17:52:20 -0300

    feat: add form in page new-goal

--- src/app/new-goal/page.tsx
+++ src/app/new-goal/page.tsx
@@ -1,32 +1,80 @@
+'use client'
+
 import { Stepper } from "@/components/Stepper";
-import { AdditionalCustomization } from "@/components/Steps/AdditionalCustomization";
-import { GoalDetails } from "@/components/Steps/GoalDetails";
-import { PlanningResources } from "@/components/Steps/PlanningResources";
-import { TrackingCommunication } from "@/components/Steps/TrackingCommunication";
+import { AdditionalCustomization, additionalCustomizationStepSchema } from "@/components/Steps/AdditionalCustomization";
+import { GoalDetails, goalDetailsStepSchema } from "@/components/Steps/GoalDetails";
+import { PlanningResources, planningResourcesStepSchema } from "@/components/Steps/PlanningResources";
+import { TrackingCommunication, trackingCommunicationStepSchema } from "@/components/Steps/TrackingCommunication";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  goalDetailsStep: goalDetailsStepSchema,
+  planningResourcesStep: planningResourcesStepSchema,
+  trackingCommunicationStep: trackingCommunicationStepSchema,
+  additionalCustomizationStep: additionalCustomizationStepSchema
+})
+
+export type FormData = z.infer<typeof schema>
 
 export default function NewGoalPage () {
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      goalDetailsStep: {
+        title: '',
+        description: '',
+        startDate: '',
+        expectedCompletionDate: ''
+      },
+      planningResourcesStep: {
+        category: '',
+        priority: '',
+        resources: ''
+      },
+      trackingCommunicationStep: {
+        progress: '',
+        notification: ''
+      },
+      additionalCustomizationStep: {
+        tags: '',
+        additionalNotes: ''
+      }
+    },
+    mode: 'onChange'
+  })
+
+  const handleSubmit = form.handleSubmit(formData => {
+    console.log({ formData })
+  })
+
   return (
     <div className="min-h-screen flex justify-center">
-      <Stepper 
-        steps={[
-          {
-            label: 'Goal Details',
-            content: <GoalDetails />
-          },
-          {
-            label: 'Planning and Resources',
-            content: <PlanningResources />
-          },
-          {
-            label: 'Tracking and Communication',
-            content: <TrackingCommunication />
-          },
-          {
-            label: 'Additional Customization',
-            content: <AdditionalCustomization />
-          }
-        ]}
-      />
+      <FormProvider {...form}>
+        <form onSubmit={handleSubmit}>
+          <Stepper 
+            steps={[
+              {
+                label: 'Goal Details',
+                content: <GoalDetails />
+              },
+              {
+                label: 'Planning and Resources',
+                content: <PlanningResources />
+              },
+              {
+                label: 'Tracking and Communication',
+                content: <TrackingCommunication />
+              },
+              {
+                label: 'Additional Customization',
+                content: <AdditionalCustomization />
+              }
+            ]}
+          />
+        </form>
+      </FormProvider>
     </div>
   )
 }
