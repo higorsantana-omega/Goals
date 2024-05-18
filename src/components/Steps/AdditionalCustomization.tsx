b50c2cfc09f1efcf84d37ddfaa75904422da9c9e
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-18 17:52:13 -0300

    feat: add errors and submit in additionalCustomization

--- src/components/Steps/AdditionalCustomization.tsx
+++ src/components/Steps/AdditionalCustomization.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { useFormContext } from "react-hook-form";
 import { StepHeader } from "../StepHeader";
 import { StepperFooter, StepperPreviousButton } from "../Stepper";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { FormData } from "@/app/new-goal/page";
+import { useStepperStore } from "@/stores/useStepperStore";
+import { z } from "zod";
+
+export const additionalCustomizationStepSchema = z.object({
+  tags: z.string().min(1),
+  additionalNotes: z.string().min(1)
+})
 
 export function AdditionalCustomization () {
+  const form = useFormContext<FormData>()
+
+  const nextStep = useStepperStore(state => state.nextStep)
+
   return (
     <div>
       <StepHeader
@@ -15,18 +30,34 @@ export function AdditionalCustomization () {
       <div className="space-y-4">
         <div className="space-y-2">
           <Label htmlFor="tags">Tags</Label>
-          <Input id="tags" />
+          <Input id="tags" {...form.register('additionalCustomizationStep.tags')} />
+          {form.formState.errors.additionalCustomizationStep?.tags && (
+            <small className="text-destructive">
+              {form.formState.errors.additionalCustomizationStep.tags.message}
+            </small>
+          )}
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="additionalNotes">Additional Notes</Label>
-          <Input id="additionalNotes" />
+          <Input id="additionalNotes" {...form.register('additionalCustomizationStep.additionalNotes')} />
+          {form.formState.errors.additionalCustomizationStep?.additionalNotes && (
+            <small className="text-destructive">
+              {form.formState.errors.additionalCustomizationStep.additionalNotes.message}
+            </small>
+          )}
         </div>
       </div>
     
       <StepperFooter>
-        <StepperPreviousButton />
-        <Button>
+        <StepperPreviousButton 
+          disabled={form.formState.isSubmitting}
+        />
+
+        <Button
+          disabled={form.formState.isSubmitting}
+          type="submit"
+        >
           Submit
         </Button>
       </StepperFooter>
