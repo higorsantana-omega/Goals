f8e6bafcea59b1da28983955174b567e54542d68
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-18 17:48:11 -0300

    feat: add schema and next step in tracking communication

--- src/components/Steps/TrackingCommunication.tsx
+++ src/components/Steps/TrackingCommunication.tsx
@@ -1,9 +1,32 @@
+import { z } from "zod";
 import { StepHeader } from "../StepHeader";
 import { StepperFooter, StepperNextButton, StepperPreviousButton } from "../Stepper";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useFormContext } from "react-hook-form";
+import { useStepperStore } from "@/stores/useStepperStore";
+import { FormData } from "@/app/new-goal/page";
+
+export const trackingCommunicationStepSchema = z.object({
+  progress: z.string().min(1),
+  notification: z.string().min(1)
+})
 
 export function TrackingCommunication () {
+  const form = useFormContext<FormData>()
+
+  const nextStep = useStepperStore(state => state.nextStep)
+
+  async function handleNextStep () {
+    const isValid = await form.trigger('trackingCommunicationStep', {
+      shouldFocus: true
+    })
+
+    if (isValid) {
+      nextStep()
+    }  
+  }
+
   return (
     <div>
       <StepHeader
@@ -14,18 +37,30 @@ export function TrackingCommunication () {
       <div className="space-y-4">
         <div className="space-y-2">
           <Label htmlFor="progress">Progress</Label>
-          <Input id="progress" />
+          <Input id="progress" {...form.register('trackingCommunicationStep.progress')} />
+          {form.formState.errors.trackingCommunicationStep?.progress && (
+            <small className="text-destructive">
+              {form.formState.errors.trackingCommunicationStep.progress.message}
+            </small>
+          )}
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="notification">Notification</Label>
-          <Input id="notification" />
+          <Input id="notification" {...form.register('trackingCommunicationStep.notification')} />
+          {form.formState.errors.trackingCommunicationStep?.notification && (
+            <small className="text-destructive">
+              {form.formState.errors.trackingCommunicationStep.notification.message}
+            </small>
+          )}
         </div>
       </div>
   
       <StepperFooter>
         <StepperPreviousButton />
-        <StepperNextButton />
+        <StepperNextButton
+          onClick={handleNextStep}
+        />
       </StepperFooter>
     </div>
   )
