e2b168a351f8014220b9f64476bebadebd7e8752
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-19 18:38:29 -0300

    feat: add getUser and createUser actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import { eq } from "drizzle-orm";
 import db from "../../drizzle"
 import * as schema from '../../drizzle/schema'
+import { genSaltSync, hashSync } from 'bcrypt-ts'
 
 type NewGoal = typeof schema.goals.$inferInsert;
 
@@ -13,4 +15,15 @@ export async function getGoals() {
 export async function createGoal(data: NewGoal) {
   const goal = await db.insert(schema.goals).values(data).returning()
   return goal
+}
+
+export async function getUser(email: string) {
+  return await db.select().from(schema.users).where(eq(schema.users.email, email))
+}
+
+export async function createUser(email: string, password: string) {
+  const salt = genSaltSync(10)
+  const hash = hashSync(password, salt)
+
+  return await db.insert(schema.users).values({ email, password: hash })
 }
