391cfab39647d0c44306a56604471cc1ed602adf
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-19 01:28:10 -0300

    feat: add categories and priorities enums

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,14 +1,32 @@
-import { pgTable, serial, text, date, boolean } from "drizzle-orm/pg-core";
-import { start } from "repl";
+import { pgTable, serial, text, date, boolean, pgEnum } from "drizzle-orm/pg-core";
+
+export const categories = [
+  "Health", "Fitness", "Finance", "Career", "Personal Growth",
+  "Relationships", "Hobbies", "Other"
+] as const
+
+export type Category = typeof categories[number]
+
+export const categoriesEnum = pgEnum(
+  "categories",
+  [...categories]
+)
+
+export const priorities = ['High', 'Medium', 'Low'] as const
+
+export const prioritiesEnum = pgEnum(
+  "priority",
+  [...priorities]
+)
 
 export const goals = pgTable("goals", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
-  description: text("description").notNull(),
+  description: text("description"),
   startDate: date("startDate").notNull(),
   expectedCompletionDate: date("expectedCompletionDate").notNull(),
-  category: text("category").notNull(),
-  priority: text("priority").notNull(),
+  category: categoriesEnum("category").notNull(),
+  priority: prioritiesEnum("priority").notNull(),
   resources: text("resources").notNull(),
   progress: text("progress").notNull(),
   notification: boolean("notification").notNull(),
