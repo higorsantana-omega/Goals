30a73b1f5876992f2090206733ba7d5808dbf7e1
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-19 02:28:18 -0300

    feat: add action to create goal

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,16 @@
 'use server'
 
 import db from "../../drizzle"
+import * as schema from '../../drizzle/schema'
+
+type NewGoal = typeof schema.goals.$inferInsert;
 
 export async function getGoals() {
   const goals = await db.query.goals.findMany()
   return goals
+}
+
+export async function createGoal(data: NewGoal) {
+  const goal = await db.insert(schema.goals).values(data).returning()
+  return goal
 }
