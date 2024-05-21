b83ff0f51fd6bca1722ae3ba45f2e2d6f4715590
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-21 19:24:03 -0300

    feat: actions to login and logout

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,6 +4,9 @@ import { eq } from "drizzle-orm";
 import db from "../../drizzle"
 import * as schema from '../../drizzle/schema'
 import { genSaltSync, hashSync } from 'bcrypt-ts'
+import { signIn, signOut } from "@/auth";
+import { loginSchema } from "@/types/schema";
+import { AuthError } from "next-auth";
 
 type NewGoal = typeof schema.goals.$inferInsert;
 
@@ -26,4 +29,65 @@ export async function createUser(email: string, password: string) {
   const hash = hashSync(password, salt)
 
   return await db.insert(schema.users).values({ email, password: hash })
+}
+
+const defaultValues = {
+  email: '',
+  password: '',
+ };
+ 
+ export async function login(prevState: any, formData: FormData) {
+  try {
+   const email = formData.get('email');
+   const password = formData.get('password');
+ 
+   const validatedFields = loginSchema.safeParse({
+    email: email,
+    password: password,
+   });
+ 
+   if (!validatedFields.success) {
+    return {
+     message: 'validation error',
+     errors: validatedFields.error.flatten().fieldErrors,
+    };
+   }
+ 
+   await signIn('credentials', {
+      redirectTo: '/goals',
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    });
+ 
+   return {
+    message: 'success',
+    errors: {},
+   };
+  } catch (error) {
+   if (error instanceof AuthError) {
+    switch (error.type) {
+     case 'CredentialsSignin':
+      return {
+       message: 'credentials error',
+       errors: {
+        ...defaultValues,
+        description: 'Invalid email or password. Please try again.'
+       },
+      };
+     default:
+      return {
+       message: 'unknown error',
+       errors: {
+        ...defaultValues,
+        description: error.message
+       },
+      };
+    }
+   }
+   throw error;
+  }
+ }
+ 
+ export async function logout() {
+  await signOut();
 }
