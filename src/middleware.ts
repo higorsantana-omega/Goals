9beed75458f15891274fb9367e77862c2ebadec4
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-21 19:23:14 -0300

    refactor: move middleware to src

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import { DEFAULT_REDIRECT, PUBLIC_ROUTES, ROOT } from '@/lib/routes'
-import { authConfig } from './src/auth.config'
+import { authConfig } from './auth.config'
 
 const { auth } = NextAuth(authConfig)
 
