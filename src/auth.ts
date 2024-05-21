6a8e7e8eb93df597466e029e3cbb91d021127da0
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-21 00:04:14 -0300

    feat: signIn and signOut from next-auth

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth, { User } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt-ts';
+import { getUser } from './app/actions';
+import { authConfig } from './auth.config'
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const [user] = await getUser(credentials.email as string)
+        if (!user) return null
+
+        const passwordMatch = await compare(credentials.password as string, user.password!)
+        if (!passwordMatch) return null
+
+        return {
+          id: user.id,
+          email: user.email!,
+        } as unknown as User
+      }
+    })
+  ],
+});
