6333a29e438b66613c115d0a952ba7e35ab199da
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-21 19:24:33 -0300

    fix: add width full button

--- src/components/SubmitLogin/index.tsx
+++ src/components/SubmitLogin/index.tsx
@@ -6,11 +6,13 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 
 export function SubmitLogin ({ children }: { children: React.ReactNode }) {
   const { pending } = useFormStatus()
+
   return (
     <Button
       type={pending ? 'button' : 'submit'}
       aria-disabled={pending}
       disabled={pending}
+      className="w-full"
     >
       {pending && <>
         <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
