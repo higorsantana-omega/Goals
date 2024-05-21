475ae9b5ab36a1e0d24cef0c16f647096ca88068
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-21 19:33:43 -0300

    chore: configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+
+module.exports = {
+  extends: ['next', 'prettier', 'react-app', 'react-app/jest', 'plugin:tailwindcss/recommended'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'off',
+    'testing-library/prefer-screen-queries': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true
+      }
+    ],
+    'tailwindcss/classnames-order': 'off',
+    'import/order': [
+      1,
+      {
+        groups: ['builtin', 'external', 'internal', 'sibling', 'parent', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          ...getDirectoriesToSort().map((singleDir) => ({
+            pattern: `${singleDir}/**`,
+            group: 'internal'
+          })),
+          {
+            pattern: 'react',
+            group: 'builtin'
+          },
+          {
+            pattern: 'env',
+            group: 'internal'
+          },
+          {
+            pattern: 'public/**',
+            group: 'internal',
+            position: 'after'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['internal'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
+    'import/newline-after-import': ['error', { count: 1 }]
+  }
+}
+
+function getDirectoriesToSort() {
+  const ignoredSortingDirectories = ['.git', '.next', '.vscode', 'node_modules']
+  return getDirectories(process.cwd()).filter((f) => !ignoredSortingDirectories.includes(f))
+}
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + '/' + file).isDirectory()
+  })
+}
