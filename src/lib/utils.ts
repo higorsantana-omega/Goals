6986ac8d472426cec86b87612cc7e2c883c75833
higorsantana-omega <higor.santanahs17@gmail.com>
2024-05-23 22:15:11 -0300

    feat: add function to format name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getProfileName(name: string) {
+  const [firstName, lastName] = name.split(' ')
+
+  if (firstName && lastName) {
+    return `${firstName[0].toUpperCase()}${lastName[0].toUpperCase()}`
+  }
+
+  return `${name[0].toUpperCase()}${name[1].toUpperCase()}`
+}
